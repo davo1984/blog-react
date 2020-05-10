8bb77c378cc5e4303ebee8073cd4527281874541
David Oliver <davooliver1964@gmail.com>
2020-05-09 20:51:51 -0400

    about & contact

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'reactstrap';
+import Home from './components/Home';
+// import Posts from './components/Projects;
+// import Posts from './components/Posts';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            <Container>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
+                    {/* <Route path="/projects" component={Projects} /> */}
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/about" component={About} />
+                    {/* <Route path="/posts" component={Posts} /> */}
+                </Switch>
+            </Container>
+        </main>
+    );
 }
-
 export default App;
