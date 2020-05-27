68744cf449efe64583d6e66fad35312f6073242a
davo1984 <davooliver1964@gmail.com>
2020-05-26 22:43:57 -0400

    Built JSON as a function. Print title & summary as cards.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,38 +4,12 @@ import {
     CardSubtitle, Container, Row, Col
 } from 'reactstrap';
 import SideMenu from './SideMenu';
+import postsJSON from './postsJSON';
 // import Post from './Post';
 
 const Blog = (props) => {
-    let posts = {
-        "post": [
-            {
-                "title": "Main Camp week 13, Offsite",
-                "subTitle": "",
-                "paragraph": [
-                    "What was your experience like overall in the Bootcamp?<br>This has been one massive bit of stress which has left me burnt outteetering on the edge of an episode of Major Depression. I guess in thone is never to mention such things. Well, that is just a part of me. Once I get a job and begin mixing with intelligent people, for the first timyears, I'm sure that I'll bounce right back. I always do.",
-
-                    "When you look back on this blog, where do you see yourself growing the most?<br>I grew the most in rekindling my love of programming. I had gotten away from that and had been hiding from the fact that that is what God has put me on this earth to do for a living. What stresses me out the most is the idea of not being able to find a job developing programs.",
-
-                    "Make sure your final portfolio, blog, homepage, final project, and whatever else are online and linked so future employers can learn about everything you did!<br>I hope these posts are of some interest to someone, employer or not. I had fun blogging and hope to continue."
-                ]
-            },
-            {
-                "title": "Main Camp week 5, Onsite",
-                "subTitle": "Week 9: Frameworks and who's afraid of the CLI?",
-                "paragraph": [
-                    "Why are CLIs scary?<br>What have you learned about CLIs?",
-                    "A CLI is just another tool to be used. I am enjoying relearning them and the power and convience they give anyone who uses them. GUIs are the scary programs. Who knows what they are doing with my data behind that pretty facade.",
-
-                    "Would you build a CLI? What would it do?",
-                    "I have no idea why someone would build a CLI other than for a spacific task, like a remote terminal or remote data entry.",
-
-                    "List a some ideas that without a Framework, would take you a long time to code.<br>"
-                ]
-            }
-        ]
-    }
-    console.log('posts', posts);
+    const posts = postsJSON();
+    console.log('posts', posts, 'length', posts.posts.post.length);
 
     const displayPost = (props) => {
         console.log('in displayPost', props);
@@ -53,22 +27,20 @@ const Blog = (props) => {
                 </header>
                 <SideMenu pageWrapId={"page-wrap"} outerContainerId={"App"} />
                 <main id="page-wrap">
-                    {/* <Row>
-                        <Col lg={{ size: 'auto', offset: 5 }} sm={{ size: 'auto', offset: 1 }} xs={{ size: 'auto' }}> */}
-                    {console.log(posts.post.length, 'posts=', posts)}
-                    {posts.post.length > 0 ? posts.post.map((post, key) =>
-                        <Card className="my-3" style={{ width: '18rem' }} onClick={() => displayPost(posts.post)}>
-                            <CardBody>
-                                <CardTitle>{post.title}</CardTitle>
-                                <CardSubtitle>{post.subTitle}</CardSubtitle>
-                                {/* {post.paragraph.length > 0 ? post.paragraph.map((paragraph, key) =>
-                                    <CardText>{paragraph}</CardText>
-                                ) : null} */}
-                            </CardBody>
-                        </Card>
-                    ) : null}
-                    {/* </Col>
-                    </Row> */}
+                    <Row>
+                        {console.log(posts.posts.post.length, 'posts=', posts.posts)}
+                        {posts.posts.post.length > 0 ? posts.posts.post.map((post, key) =>
+                            <Col lg={{ size: 3 }} sm={{ size: 'auto' }} xs={{ size: 'auto' }}>
+                                <div className="card my-3 style='width: 18rem;'" onClick={() => displayPost(posts.post)}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{post.title}</h5>
+                                        <h6 className="card-subtitle mb-2 text-muted">{post.subTitle}</h6>
+                                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                                    </div>
+                                </div>
+                            </Col>
+                        ) : null}
+                    </Row>
                 </main>
             </div>
         </Container>
---
+++ src/components/postsJSON.js
@@ -0,0 +1,168 @@
+import React from "react";
+
+const postsJSON = (props) => {
+     let posts = {
+        "post": [
+            {
+                "title": "Main Camp weeks 12 & 13, Offsite",
+                "subTitle": "",
+                "paragraph": [
+                    "What was your experience like overall in the Bootcamp?<br>This has been one massive bit of stress which has left me burnt outteetering on the edge of an episode of Major Depression. I guess in thone is never to mention such things. Well, that is just a part of me. Once I get a job and begin mixing with intelligent people, for the first timyears, I'm sure that I'll bounce right back. I always do.",
+
+                    "When you look back on this blog, where do you see yourself growing the most?<br>I grew the most in rekindling my love of programming. I had gotten away from that and had been hiding from the fact that that is what God has put me on this earth to do for a living. What stresses me out the most is the idea of not being able to find a job developing programs.",
+
+                    "Make sure your final portfolio, blog, homepage, final project, and whatever else are online and linked so future employers can learn about everything you did!<br>I hope these posts are of some interest to someone, employer or not. I had fun blogging and hope to continue."
+                ]
+            },
+            {
+                "title": "Main Camp Week 11 (15th overall), Offsite",
+                "subTitle": "",
+                "paragraph": [
+                    "Working on my final project has been equal parts wonderful and frustrating. The first week I accomplished everything I planned for myself. The second week my computer completely borked, it had been randomly rebooting but now this had become just too often. I attempted to erase the drive and reinstall everything and it shutdown before I could even finish reinstalling all the dev tools. I picked up a loaner from A-Inc and struggled. Between updating software and coding problems I never completed near as many stories as I felt I should have. This put me WAY behind.",
+                    "I try to pseudo-code every bit of code I write. Sometimes it is not much, maybe just a wireframe of how I want it to look, a diagram of how the code must flow, or a list of comments in the code of what needs to be done and how. This really does speed up my coding."
+                ]
+            },
+            {
+                "title": "Main Camp , Offsite",
+                "subTitle": "Week 10",
+                "paragraph": [
+                    "What are your thoughts about the Final Project?",
+                    "What do you think you will struggle with in completing the Final Project? Where will you excel?",
+                    "What are your opinions on the Agile Methodology? Do you think it takes up time? Do you think large scale projects can be completed without it?",
+                    "Compared to weeks three and six, what are three ideas you might want to do for your final project? Are they the same, or have they changed now that you understand more about the technology and your limitations?",
+                    "What cool new features have you learned about from the languages and frameworks you already know that will improve your workflow / add slick features to your final project?",
+                    "How many jobs have you applied to?",
+                    "Look back on the old questions to see if any of them would be good to answer this week compared to week 1."
+                ]
+            },
+            {
+                "title": "Main Camp Week 9, Offsite",
+                "subTitle": "",
+                "paragraph": [
+                    "What are your thoughts about Laravel and React?",
+                    "What is one thing that you love about React or Laravel that you used to hate / didn't understand how to use?",
+                    "",
+                    ""
+                ]
+            },
+            {
+                "title": "Main Camp 8, Offsite",
+                "subTitle": "",
+                "paragraph": [
+                    "What are your thoughts about how far you have come? How has your Digital Literacy, Computational Thinking, and Adoption of Coding Culture increased?",
+                    "How do you plan on continuing to grow those areas?",
+                    "What are you struggling with the most in the Tech Stack so far and why?",
+                    "What were your main takeaways from the career fair?",
+                    "After making it through the tech stack, where do you think your current gaps are?"
+                ]
+            },
+            {
+                "title": "Main Camp Week 7, Offsite",
+                "subTitle": "PHP, how a programming language is meant to be. This is where I feel the most comfortable. Too bad that besides from being old and unfashionable it is not a framework.",
+                "paragraph": [
+                    "What have you learned about PHP and server side programming that you were not aware of prior to this week?",
+                    "What do you want to know more about PHP and server side programming?",
+                    "How has learning PHP been? What are some similarities and differences you can see with JS",
+                    "What if you had started with PHP instead of JS?",
+                    "Are there any exercises you think would be improved if you refactored them to PHP from JS? Do you plan to?",
+                    "Find one library, framework, NPM package, and/or external web API you think you might want use for your final project."
+                ]
+            },
+            {
+                "title": "Main Camp week 6, Onsite",
+                "subTitle": "Week 10: React and how I love it and love to hate it.",
+                "paragraph": [
+                    "What do you like about React?",
+                    "What are you struggling with the most about React?",
+                    "How has React caused you to rethink your existing projects?",
+                    "What is your biggest win this week? Halfway through!",
+                    "Compared to week three, what are three ideas you might want to do for your final project? Are they the same, or have they changed now that you understand more about the technology and your limitations?"
+                ]
+            },
+            {
+                "title": "Main Camp Week 5, Offsite",
+                "subTitle": "Week 9: Frameworks. Who's afraid of the CLI? Feelings of dispair and hopelessness brought about from trying to learn to code.",
+                "paragraph": [
+                    "Why are CLIs scary?<br>What have you learned about CLIs?",
+                    "A CLI is just another tool to be used. I am enjoying relearning them and the power and convience they give anyone who uses them. GUIs are the scary programs. Who knows what they are doing with my data behind that pretty facade.",
+                    "Would you build a CLI? What would it do?",
+                    "I have no idea why someone would build a CLI other than for a spacific task, like a remote terminal or remote data entry.",
+                    "List a some ideas that without a Framework, would take you a long time to code.",
+                    "How do you feel now (week 5) compared to when you started?I'm beginning to feel despondent. I seem unable to make any forward progress. I know what I want to do but reading documentation and searching Google don't seem to be very helpful. I read the words but don't seem to be able to actually get any new stuff to work. I got the Axios call working but was unable to actually do anything with the informaLocal storage is a complete mystery to me. I despair of a better way of entering information to this blog. Creating a new page every week stinks. Time to face the facts, I will never get a job as a programmer. I am a complete career failure."
+                ]
+            },
+            {
+                "title": "Main Camp Week 4, (Overall week 8) Offsite",
+                "subTitle": "",
+                "paragraph": [
+                    "Why do you see JavaScript Frameworks and Libraries useful or hard to use?",
+                    "I don't find the React being hard to use, just totally impossible for me at the moment.  With JSX I find the syntax fiendishly finicky.  The mind shift required to transition to object oriented with high levels of abstraction is, at its best, frustrating. But I shall persevere. I just won't enjoy the process.",
+                    "The one emerging JavaScript tool that I would like to learn more about is C#! I know that it is a competing product but the language is so popular that learning it should give me a leg up on finding a job. The ability to use one language to write a program which can run on desktop computers, Android phones, as well as Apple phones and computers is too great to be ignored. My plan it to learn it by rewriting all of my projects in it. After all I will most likely have a long time to improve and expand my skills in the face of the coming social and economic recession, hopefully not a real depression.",
+                    "The COVID-19 virus forced the Bootcamp to transition to on-line instruction Wednesday at noon.  That was the right thing to do, but I am not progressing whatsoever at the moment.  I'm trying to read something from the documentation and put my understanding of it into code. That has been a total disaster. I can't seem to get anything to work and waste hours trying to get code to work in the wrong file or in the wrong manner.  This leaves me mentally exhausted to the point that I can't keep track of what I'm trying to do. A total waste of my and my instructor's time. I've spent two entire days trying to display a TicTacToe board using two loops and don't have a single line of code to show for my effort.",
+                    "A further worry for me is my MacBook Pro.  All day Friday it was overheating most severely. Several times the sound went out or it shut down due to either the heat or problems caused by the heat. I reset the PRAM and SMC chips several times and left it shutdown overnight. Saturday it gave me only one problem and Sunday it overheated only once. I performed the built in hardware diagnostics check twice without any problems being found. I'm still worried.",
+                    "But I am not defeated. I will not quit. I'll blow out my brains before I drop out. The only way I made it through almost every day of my last four years at Amazon was making plans to kill myself everyday. That really got the anger whipped up so I could make rate, keep my job and feed my family. Although extremely frustrated and totally pessimistic at the moment I also feel more alive than I have felt in years. Programming is what God made me for. It's the only job or hobby that I was ever somewhat successful at. I wish Ernie didn't get his business stolen from him, he didn't pay that great but he was a great person to work with and for.",
+                    "God I feel defeated. Is this depression coming on? No, not as long as I see a path forward."
+                ]
+            },
+            {
+                "title": "Main Camp , Offsite",
+                "subTitle": "",
+                "paragraph": [
+                    "",
+                    ""
+                ]
+            },
+            {
+                "title": "Main Camp , Offsite",
+                "subTitle": "",
+                "paragraph": [
+                    "",
+                    ""
+                ]
+            },
+            {
+                "title": "Welcome to the classroom",
+                "subTitle": "",
+                "paragraph": [
+                    "",
+                    ""
+                ]
+            },
+            {
+                "title": "Prework Week 4",
+                "subTitle": "",
+                "paragraph": [
+                    "",
+                    ""
+                ]
+            },
+            {
+                "title": "Prework Week 3",
+                "subTitle": "",
+                "paragraph": [
+                    "",
+                    ""
+                ]
+            },
+            {
+                "title": "Prework Week 2",
+                "subTitle": "",
+                "paragraph": [
+                    "",
+                    ""
+                ]
+            },
+            {
+                "title": "The preperation begins!",
+                "subTitle": "",
+                "paragraph": [
+                    "",
+                    ""
+                ]
+            }
+        ]
+    }
+    console.log('in postsJSON', posts);
+    return ( {posts} );
+};
+export default postsJSON;
