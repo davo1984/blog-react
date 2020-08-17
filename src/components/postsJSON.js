1f072629c9e833c0b459ad7139155721539064a3
David Oliver <davooliver1964@gmail.com>
2020-08-16 21:43:06 -0400

    weekly blog post

--- src/components/postsJSON.js
+++ src/components/postsJSON.js
@@ -26,6 +26,16 @@ const postsJSON = (props) => {
             //     ]
             // },
             {
+                "title": "Post-Camp Week 10",
+                "subTitle": "",
+                "paragraph": [
+                    `More plowing my way through the world of writing WordPress themes. I've got a major problem with my practice theme, but it seems to be one problem which shows up on many pages. I think I was warned about this and just need to decide to do the display by one method or another. Using both methods at the same time yields two different pages displayed on many pages. That is easy to correct. When I'm finished I'll go back and review that part.`,
+                    `I'm going to do another bit in ReactJS just to keep sharp with it. I think a hangman game. Sort of a one day project so I've something new to display. So much to learn and if I don't review it on a regular basis it goes right out of my head. That means back to CodeWars several times every week. I'll shoot for an hour a day.`,
+                    `Kenton County Library's job seeker's group "Northern Kentucky Accountability Group" has the best resources. I think I'd be happy living in Erlanger.`,
+                    `I need to be putting in a more application every week. More important is to get some websites together and hang out my own shingle: "Doc Oliver will fix yer sick website 'til itz healthy and fit as a fiddle."`
+                ]
+            },
+            {
                 "title": "Post-Camp Week 9",
                 "subTitle": "Starting a Deep Dive into WordPress",
                 "paragraph": [
